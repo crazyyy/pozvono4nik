b15be85e0f9e63b06a2c981a4940e0e60cea81f2
crazyyy <crazyyy@gmail.com>
2016-06-05 13:51:22 +0300

    footer ready

--- wordpress/wp-content/themes/wp-pozvonok/footer.php
+++ wordpress/wp-content/themes/wp-pozvonok/footer.php
@@ -8,28 +8,27 @@
 
       <div class="tags-block col-md-12">
         <h6>все по теме / ТЕГИ:</h6>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum in temporibus magnam, molestias reprehenderit, eligendi, obcaecati tenetur fugit blanditiis quam pariatur qui sapiente asperiores tempore quos. Asperiores, dignissimos, fugiat. Fugit?
+        <?php wp_tag_cloud('smallest=10&largest=10&number=0&format=list'); ?>
       </div><!-- /.tags-block-container col-md-12 -->
 
       <div class="copy-block col-md-12">
-        <h5 class="copy-block__title">Информация про раздела</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
+        <h5 class="copy-block__title"><?php the_field('footer_title', 22); ?></h5>
+        <?php the_field('footer_content', 22); ?>
       </div><!-- /.copy-block col-md-12 -->
 
-      <div class="col-md-4 footer-bottom footer-bottom__left">
+      <div class="col-md-5 footer-bottom footer-bottom__left">
         <p><span>&copy; <?php echo date("Y"); ?> Портал Pozvonochnikpro.ru</span>Копирование материалов разрешено только с указанием активной ссылки на первоисточник</p>
       </div><!-- /.col-md-4 footer-bottom footer-bottom__left -->
       <div class="col-md-4 footer-bottom footer-bottom__mid">
         <?php wpeFootNav(); ?>
         <p class="email">E-mail: <a href="mailto:info@pozvonochnikpro.ru">info@pozvonochnikpro.ru</a></p>
       </div><!-- /.col-md-4 footer-bottom footer-bottom__mid -->
-      <div class="col-md-4 footer-bottom footer-bottom__right">
+      <div class="col-md-3 footer-bottom footer-bottom__right">
         <ul class="socials">
-          <li><a href="">facebook.com</a></li>
-          <li><a href="">vk.com</a></li>
-          <li><a href="">ok.ru</a></li>
-          <li><a href="">youtube.com</a></li>
+          <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i>facebook.com</a></li>
+          <li><a href=""><i class="fa fa-vk" aria-hidden="true"></i>vk.com</a></li>
+          <li><a href=""><i class="fa fa-odnoklassniki" aria-hidden="true"></i>ok.ru</a></li>
+          <li><a href=""><i class="fa fa-youtube" aria-hidden="true"></i>youtube.com</a></li>
         </ul>
       </div><!-- /.col-md-4 footer-bottom footer-bottom__right -->
 
